b330a8ef9de46e7ff9fed22e021e83c53064c46d
Linet <teweilinet@gmail.com>
2024-03-20 14:53:49 +0300

    functions continued

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun(activity="roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+    return 'This Saturday, I want to bathe my dog!'
+}
+
+function mondayWork(activity="go to the office"){
+    return `This Monday, I will ${activity}.`
+    return 'This Monday, I will work from home.'
+}
+
+function wrapAdjective (pretty="*"){
+   return  function (adjective = 'a hard worker'){
+        return `You are ${pretty}${adjective}${pretty}!`
+    }
+    
+}
